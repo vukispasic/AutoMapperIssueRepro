f4cad2e6495b6f4bb1256afb62780561b42a4f19
vukas <vukas@DESKTOP-IU87N26>
2016-12-30 12:40:07 +0100

    Initialize updated. Instances of Entity 1 reference to same objects now. This is to reproduce scenario from ASP.NET app

--- AutoMapperRepro/Repro/Initialize.cs
+++ AutoMapperRepro/Repro/Initialize.cs
@@ -9,49 +9,47 @@ namespace Repro
 {
     public class Initialize
     {
+        Entity2 appointmentStatusHistory1;
+        Entity8 center1;
+        Entity12 insurance1;
+        Entity14 patient1;
+        Entity17 resource1;
+        Entity20 service1;
+        Entity22 speciality1;
+
         public Initialize()
         {
+            appointmentStatusHistory1 = new Entity2 { Id = Guid.NewGuid() };
+            center1 = new Entity8 { Id = Guid.NewGuid() };
+            insurance1 = new Entity12 { Id = Guid.NewGuid() };
+            patient1 = new Entity14 { Id = Guid.NewGuid() };
+            resource1 = new Entity17 { Id = Guid.NewGuid() };
+            service1 = new Entity20 { Id = Guid.NewGuid() };
+            speciality1 = new Entity22 { Id = Guid.NewGuid() };
+
+            speciality1.Entities20.Add(service1);
+
+            service1.Entities8.Add(center1);
+            service1.Entities12.Add(insurance1);
+            service1.Entities17.Add(resource1);
+            service1.Entity22 = speciality1;
+            service1.Entity22Id = speciality1.Id;
+
+            resource1.Entities8.Add(center1);
+            resource1.Entities20.Add(service1);
+
+            patient1.Entities12.Add(insurance1);
+
+            insurance1.Entities14.Add(patient1);
+            insurance1.Entities20.Add(service1);
+
+            center1.Entities17.Add(resource1);
+            center1.Entities20.Add(service1);
+            center1.Entities22.Add(speciality1);
         }
 
         public Entity1 GenerateAppointment()
-        {
-            
-            Entity2 appointmentStatusHistory1;
-            Entity8 center1;            
-            Entity12 insurance1;            
-            Entity14 patient1;            
-            Entity17 resource1;            
-            Entity20 service1;            
-            Entity22 speciality1;
-            
-            appointmentStatusHistory1 = new Entity2 { Id = Guid.NewGuid() };            
-            center1 = new Entity8 { Id = Guid.NewGuid() };            
-            insurance1 = new Entity12 { Id = Guid.NewGuid() };            
-            patient1 = new Entity14 { Id = Guid.NewGuid() };            
-            resource1 = new Entity17 { Id = Guid.NewGuid() };            
-            service1 = new Entity20 { Id = Guid.NewGuid() };            
-            speciality1 = new Entity22 { Id = Guid.NewGuid() };
-            
-            speciality1.Entities20.Add(service1);
-            
-            service1.Entities8.Add(center1);            
-            service1.Entities12.Add(insurance1);            
-            service1.Entities17.Add(resource1);            
-            service1.Entity22 = speciality1;            
-            service1.Entity22Id = speciality1.Id;
-            
-            resource1.Entities8.Add(center1);            
-            resource1.Entities20.Add(service1);
-            
-            patient1.Entities12.Add(insurance1);
-            
-            insurance1.Entities14.Add(patient1);          
-            insurance1.Entities20.Add(service1);
-         
-            center1.Entities17.Add(resource1);           
-            center1.Entities20.Add(service1);         
-            center1.Entities22.Add(speciality1);
-           
+        {                   
             var appointment1 = new Entity1 { Id = Guid.NewGuid() };          
             appointmentStatusHistory1.Entity1 = appointment1;          
             appointmentStatusHistory1.Entity1Id = appointment1.Id;
